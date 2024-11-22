8420198bd6df763de35683b7163c7c96506937a4
Cassiano de Oliveira <132411916+CassioBubanz@users.noreply.github.com>
2024-11-22 17:41:53 -0300

    Update index.jsx

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Container } from './styles'
+import React from "react";
+import { Container, ErrorCode, ErrorMessage, BackButton } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-        <h2>404</h2>
-        <p>Página não encontrada</p>
+      <ErrorCode>404</ErrorCode>
+      <ErrorMessage>Página não encontrada</ErrorMessage>
+      <p>Desculpe, não conseguimos encontrar a página que você está procurando.</p>
+      <BackButton onClick={() => navigate("/")}>Voltar para a Página Inicial</BackButton>
     </Container>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
