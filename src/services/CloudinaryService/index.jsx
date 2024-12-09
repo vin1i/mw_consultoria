3e1a9c95cf9631fe627d3d800fa7a1a09bdcd512
Cassiano de Oliveira <132411916+CassioBubanz@users.noreply.github.com>
2024-12-08 22:58:30 -0300

    Adicionando marca dagua no upload das imagens

--- src/services/CloudinaryService/index.jsx
+++ src/services/CloudinaryService/index.jsx
@@ -3,16 +3,11 @@ export const uploadImagesToCloudinary = async (files) => {
   const cloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 
   if (!uploadPreset || !cloudName) {
-    console.error("Cloudinary upload preset ou cloud name não configurado");
+    console.error("Cloudinary upload preset ou cloud name não configurados");
     return [];
   }
 
-  const filesArray = (Array.isArray(files) ? files : [files]).filter(file => file instanceof File);
-
-  if (filesArray.length === 0) {
-    console.warn("Nenhum arquivo válido para upload.");
-    return [];
-  }
+  const filesArray = Array.isArray(files) ? files : [files];
 
   const uploadPromises = filesArray.map(async (file) => {
     const formData = new FormData();
@@ -22,24 +17,15 @@ export const uploadImagesToCloudinary = async (files) => {
     try {
       const response = await fetch(
         `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-        {
-          method: "POST",
-          body: formData,
-        }
+        { method: "POST", body: formData }
       );
 
-      if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(errorText);
-      }
+      if (!response.ok) throw new Error(await response.text());
 
       const data = await response.json();
-      if (!data.public_id) {
-        console.error("Public ID não retornado pelo Cloudinary.");
-        throw new Error("Public ID não retornado.");
-      }
 
-      return data.public_id;
+      // Adiciona a transformação para marca d'água
+      return applyWatermark(data.secure_url);
     } catch (error) {
       console.error("Erro ao fazer upload para o Cloudinary:", error);
       throw error;
@@ -47,4 +33,10 @@ export const uploadImagesToCloudinary = async (files) => {
   });
 
   return await Promise.all(uploadPromises);
+};
+
+const applyWatermark = (url) => {
+  const transformation = `l_opreb9q06mnwbxsqkkey,g_north_east,x_20,y_20,w_200`;
+  const parts = url.split("/upload/");
+  return `${parts[0]}/upload/${transformation}/${parts[1]}`;
 };
