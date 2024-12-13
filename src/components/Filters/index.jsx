69006ba1d13c53901f3520fe79ebc57798331d70
Vini <126819626+vin1i@users.noreply.github.com>
2024-12-13 11:21:32 -0300

    Mudanças na tabela de preço nos filtros

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -54,14 +54,14 @@ const Filters = ({ filters, onFilterChange, filterOptions }) => {
           onChange={(e) => handleChange('precoMinimo', e.target.value)}
         >
           <option value="">Selecione o preço mínimo</option>
-          <option value={100000}>100.000</option>
-          <option value={200000}>200.000</option>
-          <option value={300000}>300.000</option>
-          <option value={400000}>400.000</option>
-          <option value={500000}>500.000</option>
-          <option value={1000000}>1.000.000</option>
-          <option value={10000000}>10.000.000</option>
-          
+    <option value={100000}>100.000</option>
+    <option value={200000}>200.000</option>
+    <option value={300000}>300.000</option>
+    <option value={500000}>500.000</option>
+    <option value={1000000}>1.000.000</option>
+    <option value={2000000}>2.000.000</option>
+    <option value={5000000}>5.000.000</option>
+    <option value={10000000}>10.000.000</option>
         </Select>
       </FieldContainer>
 
@@ -74,13 +74,13 @@ const Filters = ({ filters, onFilterChange, filterOptions }) => {
           onChange={(e) => handleChange('precoMaximo', e.target.value)}
         >
           <option value="">Selecione o preço máximo</option>
-          <option value={100000}>100.000</option>
-          <option value={200000}>200.000</option>
-          <option value={300000}>300.000</option>
-          <option value={500000}>500.000</option>
-          <option value={700000}>700.000</option>
-          <option value={1000000}>1.000.000</option>
-          <option value={20000000}>20.000.000</option>
+    <option value={500000}>500.000</option>
+    <option value={1000000}>1.000.000</option>
+    <option value={2000000}>2.000.000</option>
+    <option value={5000000}>5.000.000</option>
+    <option value={10000000}>10.000.000</option>
+    <option value={15000000}>15.000.000</option>
+    <option value={20000000}>20.000.000</option>
         </Select>
       </FieldContainer>
 
